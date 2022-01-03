50aec2b68af8ae7b4025fcaaa35caf0f78c02a7f
xsadia <lipearthur@hotmail.com>
2022-01-03 14:25:42 -0300

    feat: make posts static

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,76 @@
+import { gql } from "@apollo/client";
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from "next";
+import Head from "next/head";
+import client from "../../../apollo-client";
+import Post from "../../components/Post";
+
+type PostType = {
+  slug: string;
+};
+
+const PostPage = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Post post={post} />
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query Posts {
+        posts {
+          slug
+        }
+      }
+    `,
+  });
+
+  const paths = data.posts.map((post: PostType) => ({
+    params: {
+      slug: post.slug,
+    },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  const { data } = await client.query({
+    query: gql`
+      query Post($slug: String!) {
+        post(where: { slug: $slug }) {
+          title
+          slug
+          description
+          content {
+            html
+          }
+          createdAt
+        }
+      }
+    `,
+    variables: { slug: context.params?.slug },
+  });
+
+  return {
+    props: {
+      post: data.post,
+    },
+  };
+};
+
+export default PostPage;
